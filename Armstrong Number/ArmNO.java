046cb2c5a906247353bf1dd11cbd25aad1938168
avinash-vk <51489449+avinash-vk@users.noreply.github.com>
2019-10-20 00:33:33 +0530

    update ARMno.java
    replaced codes with classes.

--- Armstrong Number/ArmNO.java
+++ Armstrong Number/ArmNO.java
@@ -1,23 +1,58 @@
 import java.util.*;
-class ArmNO
-{  
-      public static void main(String[] args) 
-	 {  
-	        int c=0,a,temp;  
-	        int n;
-		Scanner sc= new Scanner(System.in);
-		System.out.print("Enter the number");
-		n=sc.nextInt();
-	        temp=n;  
-	        while(n>0)  
-	        {  
-		        a=n%10;  
-		        n=n/10;  
-        		c=c+(a*a*a);  
-	        }  
-	        if(temp==c)  
-		        System.out.println(temp+" is an armstrong number");   
-	        else  
-        	   	System.out.println("Not armstrong number");   
-       }  
-}  
+public class Armstrong 
+{ 
+    /* Function to calculate x raised to the h
+       power y */
+    int power(int x, long y) 
+    { 
+        if( y == 0) 
+            return 1; 
+        if (y%2 == 0) 
+            return power(x, y/2)*power(x, y/2); 
+        return x*power(x, y/2)*power(x, y/2); 
+    } 
+  
+    /* Function to calculate order of the number */
+    int order(int x) 
+    { 
+        int n = 0; 
+        while (x != 0) 
+        { 
+            n++; 
+            x = x/10; 
+        } 
+        return n; 
+    } 
+  
+    // Function to check whether the given number is 
+    // Armstrong number or not 
+    boolean isArmstrong (int x) 
+    { 
+        // Calling order function 
+        int n = order(x); 
+        int temp=x, sum=0; 
+        while (temp!=0) 
+        { 
+            int r = temp%10; 
+            sum = sum + power(r,n); 
+            temp = temp/10; 
+        } 
+  
+        // If satisfies Armstrong condition 
+        return (sum == x); 
+    } 
+  
+    // Driver Program 
+    public static void main(String[] args) 
+    { 
+        Armstrong ob = new Armstrong(); 
+	Scanner sc= new Scanner(System.in);
+	System.out.print("Enter the number");
+	n=sc.nextInt();
+	if ob.isArmstrong(n){
+		System.out.println(temp+" is an armstrong number");
+	}
+	else
+		System.out.printlnm(temp+"is not an armstrong number");
+       } 
+}
